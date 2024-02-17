90ec4b42bb8a0619eddea2bf6c6b60efab034dc7
Sam Burdick <samuelmburdick@gmail.com>
2024-02-16 22:19:57 -0800

    File write

--- example.py
+++ example.py
@@ -4,8 +4,13 @@ import pymatching
 
 circuit = stim.Circuit.generated("surface_code:rotated_memory_x", distance=5, rounds=5, after_clifford_depolarization=0.005)
 
+f1 = open( "model", "w")
+f2 = open("matching", "w")
 model = circuit.detector_error_model(decompose_errors=True)
+f1.write(str(model))
 matching = pymatching.Matching.from_detector_error_model(model)
+f2.write(str(matching))
+
 
 sampler = circuit.compile_detector_sampler()
 
@@ -17,5 +22,6 @@ for i in range(syndrome.shape[0]):
     num_errors += not np.array_equal(actual_observables[i, :], predicted_observables)
 
 print(num_errors)
-print()
-print(syndrome, actual_observables)
+
+for f in [f1, f2]:
+    f.close()
